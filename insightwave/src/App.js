c697f56c35b027d62d7ca4089e2fd8c6e47d370d
thatkhay <johnkingsleyegeonu@gmail.com>
2023-05-31 21:48:51 +0100

    feedback ui

--- insightwave/src/App.js
+++ insightwave/src/App.js
@@ -1,12 +1,12 @@
 
 import Header from "./component/Header"
-
+import FeedbackItem from "./component/FeedbackItem"
 function App() {
 return(
 <>
   <Header />
     <div className="container">
-       
+    <FeedbackItem />
     </div>
 </>
   
